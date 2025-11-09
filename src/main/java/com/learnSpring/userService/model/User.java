c900d6b6fc772a3892df23636529d4e89f08ac23
nilanjana07 <nilanjana030807@gmail.com>
2025-11-10 01:25:31 +0530

    Created UserService

---
+++ src/main/java/com/learnSpring/userService/model/User.java
@@ -0,0 +1,24 @@
+package com.learnSpring.userService.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private String email;
+        private int age;
+
+}
+
+
