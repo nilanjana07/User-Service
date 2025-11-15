fbde5f3b85d258a7781da2b7cf183d55a7b1346d
nilanjana07 <nilanjana030807@gmail.com>
2025-11-15 23:53:52 +0530

    removing unnecessary duplicates

--- src/main/java/com/learnSpring/userService/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.learnSpring.userService.repository;
-import com.learnSpring.userService.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-public interface UserRepository extends JpaRepository<User, Long> {
-}
